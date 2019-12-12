b494c479fb7a0d1e5e244311264618cc9ffbaca2
Jakub Král <kubus415@gmail.com>
2019-12-12 15:14:14 +0100

    Připrava Menu

--- src/index.php
+++ src/index.php
@@ -7,6 +7,27 @@
     <title>Document</title>
 </head>
 <body>
-    
+<nav class="nav">
+        <div a href="index.php?page=home">PVY1 Helpdesk</a></div>
+        <ul> 
+            <li a href="index.php?page=predmety">Předměty</a></li>
+        </ul>
+    </nav>
+
+<?php
+    $main = $_GET["page"];
+    switch ($main) {
+        case "home":
+            include "home.php";
+            break;
+        case "predmety":
+            include "predmety.php";
+            break;
+        default:
+            include "home.php";
+            break;
+    }
+?>
+
 </body>
 </html>
