d832a892fb2c11955802787b39d405b7d97a6f22
Jakub Král <kubus415@gmail.com>
2019-12-14 14:00:14 +0100

    update-menu
    Menu podle templatu + úprava od Hanušky

--- src/index.php
+++ src/index.php
@@ -1,21 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css" />
     <title>Document</title>
 </head>
-<body>
-<nav class="nav">
-        <div a href="index.php?page=home">PVY1 Helpdesk</a></div>
-        <ul> 
-            <li a href="index.php?page=predmety">Předměty</a></li>
-        </ul>
-    </nav>
 
-<?php
-    $main = $_GET["page"];
+<body>
+    <div class="wrap">
+        <nav>
+            <ul class="primary">
+                <li><a href="index.php?page=home">PVY1 Helpdesk</a></li>    
+                <li>
+                    <a href="index.php?page=predmety">Předměty</a>
+                    <ul class="sub">
+                        <li><a href="">PVY</a></li>
+                        <li><a href="">POG</a></li>
+                        <li><a href="">PRG</a></li>
+                        <li><a href="">POS</a></li>
+                    </ul>
+                </li>
+                <li><a href="index.php?page=onas">O nás</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <?php
+    if (isset($_GET['Page'])) {
+        $main = $_GET['Page'];
+    } else {
+        $main = "home";
+    }
     switch ($main) {
         case "home":
             include "home.php";
@@ -23,11 +41,16 @@
         case "predmety":
             include "predmety.php";
             break;
+        case "onas":
+            include "onas.php";
+            break;
         default:
             include "home.php";
             break;
     }
-?>
+
+    ?>
 
 </body>
+
 </html>
