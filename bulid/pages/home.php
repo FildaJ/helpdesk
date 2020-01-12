3d3ce64b7c8989ec860f9a628d4f428b86ff00e4
Filip <filip.just.01@gmail.com>
2020-01-12 12:00:41 +0100

    Přidán testovací text
    Testovacíí text na úvodní stránce slouží pro vyzkoušení typografie

--- bulid/pages/home.php
+++ bulid/pages/home.php
@@ -1,3 +1,10 @@
 <article>
-
+    <h1>Nadpis 1</h1>
+    <h2>Nadpis 2</h2>
+    <h3>Nadpis 3</h3>
+    <h4>Nadpis 4</h4>
+    <h5>Nadpis 5</h5>
+    <p>Autem ut architecto et provident eaque sed eveniet est. Non cupiditate reiciendis voluptas qui vitae dignissimos eveniet alias. A quos animi ex est voluptatem qui sit. Expedita eos eos perferendis similique porro. Autem quasi neque a harum non est. Et et dolorum eum sed vel suscipit quisquam aut.
+	</p>
+    
 </article>
