f6327c7471d5caf38512e5df925063796ac11051
SirHanus <kubik.2001@seznam.cz>
2020-01-14 11:04:32 +0000

    Update join.php

--- bulid/pages/join.php
+++ bulid/pages/join.php
@@ -3,9 +3,10 @@ session_start();
 $servername = "localhost";
 $username = "matura";
 $password = "Maturita2020";
+$dbname = "helpdesk";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
