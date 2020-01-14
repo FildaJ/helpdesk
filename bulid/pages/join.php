ec2b709b43979093207ad6fc1486d3e8a80bbab1
SirHanus <kubik.2001@seznam.cz>
2020-01-14 10:13:54 +0000

    join file upload

---
+++ bulid/pages/join.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "helpdesk";
+$username = "matura";
+$password = "Maturita2020";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
