0d6ba75d520f8363f094e3a5645f09df6f379ac8
Filip <filip.just.01@gmail.com>
2020-02-05 22:32:30 +0100

    Úprava tlačítek
    Nyní jsou tlačítka pouze odkazy s příslušnou třídou, namísto div s třídou, ve kterém byl odkaz.

--- bulid/pages/home.php
+++ bulid/pages/home.php
@@ -5,7 +5,7 @@
 	<p>Autem ut architecto et provident eaque sed eveniet est. Non cupiditate reiciendis voluptas qui vitae dignissimos eveniet alias. A quos animi ex est voluptatem qui sit. Expedita eos eos perferendis similique porro. Autem quasi neque a harum non est. Et et dolorum eum sed vel suscipit quisquam aut.
 	</p>
 	<div class="buttonBox">
-		<div class="btn-primary"><a href="">Stiskni mě</a></div>
-		<div class="btn-ghost"><a href="">Stiskni mě</a></div>
+		<a class="btn-primary" href="">Stiskni mě</a>
+		<a class="btn-ghost" href="">Stiskni mě</a>
 	</div>
 </article>
