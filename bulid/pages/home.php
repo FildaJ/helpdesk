25d4d9ac29aa275e039b15018918b21fa8721ddf
Filip <filip.just.01@gmail.com>
2020-02-06 23:34:50 +0100

    Rozšířeny styly typografie + Přidán copywriting na úvodní stránku

--- bulid/pages/home.php
+++ bulid/pages/home.php
@@ -1,15 +1,29 @@
 <header>
-	<h1>Ahoj</h1>
+	<h1>Maturita jednoduše</h1>
 	<div class='heroImg' style="background-image(img/header/home.png);"></div>
 </header>
-<article class="centerContainer">
-	<h1>Nadpis 1</h1>
-	<h2>Nadpis 2</h2>
-	<h3>Nadpis 3</h3>
-	<p>Autem ut architecto et provident eaque sed eveniet est. Non cupiditate reiciendis voluptas qui vitae dignissimos eveniet alias. A quos animi ex est voluptatem qui sit. Expedita eos eos perferendis similique porro. Autem quasi neque a harum non est. Et et dolorum eum sed vel suscipit quisquam aut.
-	</p>
-	<div class="buttonBox">
-		<a class="btn-primary" href="">Stiskni mě</a>
-		<a class="btn-ghost" href="">Stiskni mě</a>
-	</div>
-</article>
+<main class="centerContainer">
+	<article>
+		<h2>Maturita za rohem...</h2>
+		<p>
+			...a nemáš se z čeho učit? Tak to jsi zavítal na správné místo. Na našem webu můžete najít všechny možné témata z různých maturitních předmětů. Vše je tady, na jednom místě. Stačí několikrát kliknout a můžeš se začít připravovat.
+		</p>
+	</article>
+
+	<article>
+		<h2>Nejsi velký čtenář?</h2>
+		<p>Povinou četbu k maturitě za tebe (zatím) bohužel nepřečteme, ale jinak s přípravou k maturitě s minimem čtení není vůbec problém. K některým tématům jsou k dispozici stručná výuková videa a přehledné infografiky.</p>
+	</article>
+
+	<article>
+		<h2>Přilož ruku k dílu</h2>
+		<p>Umíš programovat, jsi dobrý na design nebo umíš výborně vysvětlovat věci? I ty můžeš přispět do tohoto projektu svou pomocí. Stačí navštívit náš repozitář na Gitlabu, kde jsou veškeré zdrojové kódy k této stránce a všechny výukové materiály.</p>
+		<a href="https://gitlab.com/snoopcat/helpdesk" class="btn-ghost">Gitlab</a>
+	</article>
+
+	<article>
+		<p>sdlkjfs jksjdlf jlkdsj fljsdlfj lsaj fd</p>
+		<blockquote>sdfsajd lkjsdaůl fjlsd jlkfjsaůd js</blockquote>
+		<p>sdfs sjdůf jdsaůkj flsjd ůjsů la</p>
+	</article>
+<main>
