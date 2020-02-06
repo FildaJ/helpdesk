c88cc48a013601e1690993108789a2d4286c8230
Filip <filip.just.01@gmail.com>
2020-02-06 21:17:24 +0100

    Zápatí pro desktop

--- bulid/scripts/footer.php
+++ bulid/scripts/footer.php
@@ -1,2 +1,8 @@
 <footer>
+	<div class="centerContainer">
+		<a href="http://forum-matura.jednoduse.cz" target="_blank">Diskuzní forum</a>
+		<a href="https://gitlab.com/snoopcat/helpdesk" target="_blank">Gitlab repozitář</a>
+		<span><strong>Vytvořili:</strong> Kuba, Roman, Kuba, Filip, Kuba, Tom, Kuba, Kuba, Franta a Matěj </span>
+		<span>&#169; Matura Jednoduše 2019 - <?php echo date("Y"); ?></span>
+	</div>
 </footer>
