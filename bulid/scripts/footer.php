79cfcb57e4999d4e6d737dcb7ee7e69f477c73aa
Filip <filip.just.01@gmail.com>
2020-02-07 08:24:31 +0100

    přidaná dokumentace

--- bulid/scripts/footer.php
+++ bulid/scripts/footer.php
@@ -2,6 +2,7 @@
 	<div class="centerContainer">
 		<a href="http://forum-matura.jednoduse.cz" target="_blank">Diskuzní forum</a>
 		<a href="https://gitlab.com/snoopcat/helpdesk" target="_blank">Gitlab repozitář</a>
+		<a href="Dokumentace projektu.docx">Dokumentace</a>
 		<span><strong>Vytvořili:</strong> Kuba, Roman, Kuba, Filip, Kuba, Tom, Kuba, Kuba, Franta a Matěj </span>
 		<span>&#169; Matura Jednoduše 2019 - <?php echo date("Y"); ?></span>
 	</div>
