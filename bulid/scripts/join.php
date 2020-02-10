f5acdf02567860b978495e23df843ef47a6825e1
Filip <filip.just.01@gmail.com>
2020-02-10 19:59:54 +0100

    Úprava typografie

--- bulid/scripts/join.php
+++ bulid/scripts/join.php
@@ -1,15 +1,15 @@
 <?php
-
+/*
 $servername = "localhost";
 $username = "matura";                 //endora
 $password = "Maturita2020";
 $dbname = "helpdesk"; 
-/*
+
 $servername = "localhost";
 $username = "root";                 //xamp
 $password = "";
 $dbname = "helpdesk";              
-*/
+
 
 
 // Create connection
@@ -19,5 +19,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
+*/
 ?>
