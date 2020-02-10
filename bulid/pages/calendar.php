68a029f5f1f5b1cdfa4c9472a5e29b787e60fcf9
Filip <filip.just.01@gmail.com>
2020-02-10 20:42:54 +0100

    Úprava kalendáře

--- bulid/pages/calendar.php
+++ bulid/pages/calendar.php
@@ -1,8 +1,9 @@
 <main>
 	<div id="calendar">
 		<link href="css/calendar.css" type="text/css" rel="stylesheet" />
+		<link href="https://demo.phpjabbers.com/1580993911_668/core/framework/libs/pj/css/pj.bootstrap.min.css" type="text/css" rel="stylesheet" />
 		<link href="https://demo.phpjabbers.com/1580993911_668/index.php?controller=pjFront&action=pjActionLoadCss&theme=8" type="text/css" rel="stylesheet" />
-		<script type="text/javascript" src="https://demo.phpjabbers.com/1580993911_668/index.php?controller=pjFront&action=pjActionLoad&theme=8&view=list&icons=F&cats=F">
+		<script type="text/javascript" src="https://demo.phpjabbers.com/1580993911_668/index.php?controller=pjFront&action=pjActionLoad&theme=8&view=calendar&icons=F&cats=F"></script>
 		</script>
 	</div>
 </main>
